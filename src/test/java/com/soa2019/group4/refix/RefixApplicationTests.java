5af0219b05d351fd5e7927d6fd4490f59b2a85e0
mickey0142 <59070142@it.kmitl.ac.th>
2019-03-11 12:04:21 +0700

    initialize project and add request service

---
+++ src/test/java/com/soa2019/group4/refix/RefixApplicationTests.java
@@ -0,0 +1,16 @@
+package com.soa2019.group4.refix;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RefixApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
