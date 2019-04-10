c4f63d8288c45f8ffba2e2557ecbf4f71a5498e1
surachatyaitammasan <59070180@it.kmitl.ac.th>
2019-04-10 14:50:34 +0700

    add config

--- contact/start/db.js
+++ contact/start/db.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
+const config = require('config')
 module.exports = ()=>{
-    mongoose.connect('mongodb:localhost/contact', {useNewUrlParser: true})
-    .then(() => console.log('Connecting to MongoDB')
+    const db = config.get("db")
+    mongoose.connect(db, {useNewUrlParser: true})
+    .then(() => console.log('Connecting to MongoDB' + config.get("db"))
     )
 }
