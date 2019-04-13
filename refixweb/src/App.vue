e9547a628514bb585d934b940ce7172fbc56cd8a
surachatyaitammasan <59070180@it.kmitl.ac.th>
2019-04-14 01:45:10 +0700

    update web

--- refixweb/src/App.vue
+++ refixweb/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-navbar varinat="faded" type="light">
+    <b-navbar-brand href="#"><img src="./assets/refixLogo.png" fluid alt="logo" class="imgLogo"></b-navbar-brand>
+    </b-navbar>
+    <HelloWorld msg="Welcome to Refix. I will help you 24Hr."/>
   </div>
 </template>
 
@@ -17,12 +19,19 @@ export default {
 </script>
 
 <style>
+.item:hover{
+  color: black;
+  background-color: aliceblue;
+}
+img.imgLogo{
+  width: 10%;
+  height: 10%;
+}
 #app {
+  background-image: linear-gradient(120deg, #2857D8 0%, #2C92EB 100%);
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
 </style>
--- refixweb/src/components/HelloWorld.vue
+++ refixweb/src/components/HelloWorld.vue
@@ -1,58 +1,23 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="container" id="home">
+    <b-nav tabs class="container">
+    <b-col><b-nav-item class="item" active>Active</b-nav-item></b-col>
+    <b-col><b-nav-item class="item">Link</b-nav-item></b-col>
+    <b-col><b-nav-item class="item">Another Link</b-nav-item></b-col>
+    <b-col><b-nav-item class="item">Disabled</b-nav-item></b-col>
+  </b-nav>
+    <b-card
+        title="ABC Home Inc."
+        img-src="https://picsum.photos/600/300/?image=25"
+        img-alt="name inc."
+        img-top
+        style="max-width: 20rem;"
+        class="mb-2">
+      <b-card-text>ABC HOME</b-card-text>
+    </b-card>
   </div>
 </template>
-
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style>
 </style>
+
+
