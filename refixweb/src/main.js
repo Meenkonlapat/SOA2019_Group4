9a4d0a9a8265def0423a71f140aa504f6df01434
surachatyaitammasan <59070180@it.kmitl.ac.th>
2019-04-20 18:40:31 +0700

    modal status

--- refixweb/src/main.js
+++ refixweb/src/main.js
@@ -5,11 +5,13 @@ import VueRouter from 'vue-router'
 import { routes } from "./routes/route.js";
 
 import VueResource from 'vue-resource';
+import BootstrapVue from 'bootstrap-vue'
 
 import { store } from './store/store.js';
 
 Vue.use(VueRouter);
 Vue.use(VueResource);
+Vue.use(BootstrapVue);
 
 const router = new VueRouter({
   mode: 'history',
