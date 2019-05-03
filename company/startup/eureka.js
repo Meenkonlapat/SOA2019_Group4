95efb46bf23ad4433905d6fa4c0ca9d6f3dae14f
mickey0142 <59070142@it.kmitl.ac.th>
2019-05-03 22:53:02 +0700

    add eureka client to services

--- company/startup/eureka.js
+++ company/startup/eureka.js
@@ -2,27 +2,27 @@ const Eureka = require("eureka-js-client").Eureka;
 
 module.exports = function()
 {
-    const client = new Eureka({
-      instance: {
-        app: 'companyService',
-        hostName: 'localhost',
-        ipAddr: '127.0.0.1',
-        statusPageUrl: 'http://localhost:8080/info',
-        port: {
-          '$': 8080,
-          '@enabled': 'true',
+  const client = new Eureka({
+    instance: {
+      app: 'companyService',
+      hostName: 'localhost',
+      ipAddr: '127.0.0.1',
+      statusPageUrl: 'http://localhost:8080/info',
+      port: {
+        '$': 8080,
+        '@enabled': 'true',
       },
       vipAddress: 'refix.company.com',
       dataCenterInfo: {
-          '@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
+        '@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
           name: 'MyOwn',// don't change this
+        },
       },
-  },
-  eureka: {
-    host: '192.168.1.37',
-    port: 8761,
-    servicePath: '/eureka/apps/'
-},
-});
-    client.start();
+      eureka: {
+        host: '192.168.1.37',
+        port: 8761,
+        servicePath: '/eureka/apps/'
+      },
+    });
+  client.start();
 }
