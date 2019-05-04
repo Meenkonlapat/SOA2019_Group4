39b6d79b8af1ca902fa95a764d87fa96d3a64cdf
mickey0142 <59070142@it.kmitl.ac.th>
2019-05-04 15:34:26 +0700

    add chat button in admin request

--- refixweb/src/components/AdminRequest.vue
+++ refixweb/src/components/AdminRequest.vue
@@ -1,10 +1,9 @@
 <template>
   <div id="lookAllRequrst">
     <div class="container">
-
-    <router-link to="/admin"><button class="btn" id="backPage">
-      Previous
-    </button></router-link>
+      <router-link to="/admin">
+        <button class="btn" id="backPage">Previous</button>
+      </router-link>
       <table class="table table-borderless mt-5 border table-light text-center">
         <thead>
           <tr class="table-bordered">
@@ -12,6 +11,7 @@
             <th scope="col">Repair</th>
             <th scope="col">Status</th>
             <th scope="col">Bill</th>
+            <th scope="col">Chat</th>
           </tr>
         </thead>
         <tbody>
@@ -27,6 +27,9 @@
                 </router-link>
               </template>
               <template v-else>-</template>
+            </td>
+            <td>
+              <button @click="chatClicked(req)">chat</button>
             </td>
           </tr>
         </tbody>
@@ -49,9 +52,48 @@ export default {
       }
     }
   },
-  methods:{
-    setRequest(req){
+  methods: {
+    setRequest(req) {
       this.$store.dispatch("commitRequest", req);
+    },
+    chatClicked(req) {
+      this.$http
+        .get("https://contact-dot-refixsoa2019.appspot.com/api/contact")
+        .then(response => {
+          return response.json();
+        })
+        .then(data => {
+          let isRoomExisted = false;
+          for (let key in data) {
+            if (
+              data[key].companyId == this.currentUser.ID &&
+              data[key].customerId == req.customer.id
+            ) {
+              isRoomExisted = true;
+            }
+          }
+          if (!isRoomExisted) {
+            let contactObject = {
+              companyId: this.currentUser.ID,
+              companyName: this.currentUser.name,
+              customerId: req.customer.id,
+              customerName: req.customer.name,
+              chat: []
+            };
+            this.$http
+              .post(
+                "https://contact-dot-refixsoa2019.appspot.com/api/contact",
+                contactObject
+              )
+              .then(() => {
+                console.log("created room");
+                console.log(contactObject);
+                this.$router.push("/messenger");
+              });
+          } else {
+            this.$router.push("/messenger");
+          }
+        });
     }
   },
   created() {
@@ -73,10 +115,9 @@ export default {
 };
 </script>
 
-
 <style>
 #backPage {
-  margin-top: .5em;
+  margin-top: 0.5em;
   color: #ffffff;
   display: inline-block;
   font-size: 14px;
